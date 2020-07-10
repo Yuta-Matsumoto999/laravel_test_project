b17566d0c422ebf13280106ca6e33b67b5eabfc2
松本雄大 <y.matsumoto0723@gmail.com>
2020-07-10 23:48:50 +0900

    validation編集

--- project/project_app/app/Http/Requests/PurchaseRequest.php
+++ project/project_app/app/Http/Requests/PurchaseRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UserRequest extends FormRequest
+class PurchaseRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,8 +24,6 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-          'birthday' => 'required|date|before_or_equal:today',
-          'gender' => 'required',
           'address_num' => 'required|integer|digits_between:7,7',
           'address' => 'required|string'
         ];
@@ -40,8 +38,6 @@ class UserRequest extends FormRequest
     {
         return [
             'required' => '必須の項目です',
-            'date' => '形式を確認してください',
-            'before_or_equal:today' => '本日以降は指定できません',
             'string' => '文字列以外は入力できません',
             'integer' => '数字のみで入力してください',
             'digits_between' => '正しく入力してください'
