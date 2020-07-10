dd1837d96f5fbab270b3364f52273d9b8bebee66
松本雄大 <y.matsumoto0723@gmail.com>
2020-07-10 23:53:12 +0900

    route編集

--- project/project_app/routes/web.php
+++ project/project_app/routes/web.php
@@ -14,11 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 // user側
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
     Auth::routes();
+
+    Route::get('/', function () {
+        return redirect()->route('sale.index');
+    });
 
     Route::post('logout', 'Auth\LoginController@logout')->name('logout');
     Route::get('/sale', 'user\SaleController@index')->name('sale.index');
@@ -29,9 +30,12 @@ use Illuminate\Support\Facades\Route;
     Route::get('/sale/cart/purchase', 'user\SaleController@showCartPurchase')->name('sale.show.cart.purchase');
     Route::get('/sale/question', 'user\SaleController@showMycontact')->name('sale.show.myquestion');
     Route::get('/sale/{id}/question', 'user\SaleController@contact')->name('sale.show.question');
+    Route::get('/sale/buys', 'user\SaleController@showBuys')->name('sale.show.buys');
+    Route::get('/sale/user', 'user\SaleController@showUser')->name('sale.show.user');
     Route::post('/sale/product/cart/{id}', 'user\SaleController@storeCart')->name('sale.store.cart');
     Route::post('/sale/contact', 'user\SaleController@storeContact')->name('sale.store.contact');
     Route::post('/sale/purchase/cart', 'user\SaleController@storeCartPurchase')->name('sale.store.cart.purchase');
+    Route::post('/sale/user', 'user\SaleController@updateUser')->name('sale.update.user');
     Route::put('/sale/cart/{id}', 'user\SaleController@updateCart')->name('sale.update.cart');
     Route::delete('/sale/cart/destroy/{id}', 'user\SaleController@destroyByCart')->name('sale.destroy.cart');
     Route::delete('/sale/contact/{id}', 'user\SaleController@destroyContact')->name('sale.destroy.contact');
@@ -47,14 +51,17 @@ Route::prefix('admin')->namespace('admin')->name('admin.')->group(function(){
     Route::post('logout', 'Auth\LoginController@logout')->name('logout');
     Route::get('/', 'adminController@index')->name('index');
     Route::get('/users', 'adminController@showUsers')->name('users');
-    Route::get('/user/{id}', 'adminController@editUser')->name('edit.user');
+    Route::get('/user/{id}', 'adminController@confirmUser')->name('.user');
     Route::get('/product', 'adminController@showProducts')->name('products');
     Route::get('/product/{id}', 'adminController@editProduct')->name('edit.product');
     Route::get('/new/product', 'adminController@createProduct')->name('create.product');
     Route::get('/contact', 'adminController@showContacts')->name('contacts');
     Route::get('/contact/{id}', 'adminController@editComment')->name('editComment');
+    Route::get('/buys', 'adminController@showBuys')->name('buys');
+    Route::get('/buys/{id}', 'adminController@editBuys')->name('edit.buys');
     Route::post('/product/new', 'adminController@storeProduct')->name('store.product');
     Route::post('/comment/{id}', 'adminController@storeComment')->name('store.comment');
+    Route::post('/buy/{id}', 'adminController@storeShipping')->name('store.shipping');
     Route::put('/user/{id}', 'adminController@updateUser')->name('update.user');
     Route::post('/{id}/product', 'adminController@updateProduct')->name('update.product');
     Route::delete('/product/{id}', 'adminController@destroyProduct')->name('destroy.product');
